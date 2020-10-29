766d0bf402530fb73fb743df279514fe8329d83d
Pierre Barbillon <55078083+barbillo4u@users.noreply.github.com>
2020-10-29 14:42:56 +0100

    FINAL : Optimisation et commentaires
    Quelques optimisations et ajout de commentaires afin que le code soit plus lisible et compréhensible.

--- Game_of_Life/main.cpp
+++ Game_of_Life/main.cpp
@@ -1,9 +1,9 @@
 #include <SFML/Graphics.hpp>
-
 #include <cstdlib>
 
 #define DEAD 0
 #define LIVE 1
+
 #define CELL_SIZE 12
 
 #define WIDTH 110
@@ -11,6 +11,106 @@
 
 #define SCREEN_WIDTH (CELL_SIZE*WIDTH)+4
 #define SCREEN_HEIGHT (CELL_SIZE*HEIGHT)+4
+
+/**
+	Initialize state, temp array and cells grid
+*/
+void init(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][HEIGHT]);
+
+/**
+	Count the number of neighbor cells for a given cell
+*/
+int count_neighbour(bool state[][HEIGHT], int x, int y);
+
+/**
+	Clear the grid and the state
+*/
+void clear_grid(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][HEIGHT]);
+
+/**
+	Runs the game with all the rules
+*/
+void run(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][HEIGHT]);
+
+/**
+	Draw all the cell grid in the given window
+*/
+void draw_grid(sf::RenderWindow* window, sf::RectangleShape cells[][HEIGHT]);
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Game Of Life", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event event;
+	sf::Vector2i mouse_cursor;
+	bool state[WIDTH][HEIGHT] ;
+	bool temp[WIDTH][HEIGHT];
+	bool running = false;
+	int cellX, cellY;
+	sf::RectangleShape cells[WIDTH][HEIGHT];
+
+	window.setFramerateLimit(60);
+
+	init(state, temp, cells);
+
+	while(window.isOpen())
+	{
+		while(window.pollEvent(event))
+		{
+			mouse_cursor = sf::Mouse::getPosition(window);
+			cellX = mouse_cursor.x/CELL_SIZE;
+			cellY = mouse_cursor.y/CELL_SIZE;
+
+			if(event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				window.close();
+
+			/*
+				Prevents segmentation error in Release mode
+			*/
+			if(cellX == WIDTH)
+				cellX--;
+			if(cellY == HEIGHT)
+				cellY--;
+
+			if((mouse_cursor.x <= SCREEN_WIDTH && mouse_cursor.y <= SCREEN_HEIGHT) && (mouse_cursor.x > 0 && mouse_cursor.y > 0))
+			{
+				if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				{
+					cells[cellX][cellY].setFillColor(sf::Color::Black);
+					state[cellX][cellY] = LIVE;
+				}
+
+
+				if(sf::Mouse::isButtonPressed((sf::Mouse::Right)))
+				{
+					cells[cellX][cellY].setFillColor(sf::Color::White);
+					state[cellX][cellY] = DEAD;
+				}
+			}
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+				clear_grid(state, temp, cells);
+
+			if(sf::Keyboard::isKeyPressed((sf::Keyboard::Space)))
+				running = true;
+
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+			{
+				clear_grid(state, temp, cells);
+				running = false;
+			}
+
+		}
+
+		if(running)
+			run(state, temp, cells);
+
+		window.clear();
+		draw_grid(&window, cells);
+		window.display();
+	}
+
+    return EXIT_SUCCESS;
+}
 
 void init(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][HEIGHT])
 {
@@ -28,13 +128,7 @@ void init(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][
 		}
 	}
 }
-unsigned int mod(int value, unsigned int divisor)
-{
-	unsigned int mod = value % divisor;
 
-
-	return mod + (mod < 0 ? divisor : 0);
-}
 int count_neighbour(bool state[][HEIGHT], int x, int y)
 {
 	int number_neighbour = 0;
@@ -43,92 +137,98 @@ int count_neighbour(bool state[][HEIGHT], int x, int y)
 	int up;
 	int down = (y+1)%HEIGHT;
 
+	/*
+		Check for negative value with modulo, to make an infinite grid
+	*/
 	if(x-1<0)
 		left = WIDTH - (abs(x-1) % WIDTH);
 	else
 		left = (x-1)%WIDTH;
-
 	if(y-1<0)
 		up = HEIGHT - (abs(y-1) % HEIGHT);
 	else
 		up = (y-1)%HEIGHT;
+
+	/*
+		Checking of all neighbor cells
+	*/
 	//Diagonal up-left
 	if(state[left][up] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	//up
 	if(state[x][up] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	//Diagonal up-right
 	if(state[right][up] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	//left
 	if(state[left][y] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	//right
 	if(state[right][y] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	//Diagonal down-left
 	if(state[left][down] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	//down
 	if(state[x][down] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	//Diagonal down-right
 	if(state[right][down] == LIVE)
-	{
 		number_neighbour++;
-	}
 
 	return number_neighbour;
 }
 
+void clear_grid(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][HEIGHT])
+{
+	for(int x = 0; x < WIDTH; x++)
+	{
+		for(int y = 0; y < HEIGHT; y++)
+		{
+			state[x][y] = DEAD;
+			temp[x][y] = DEAD;
+			cells[x][y].setFillColor(sf::Color::White);
+		}
+	}
+}
+
 void run(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][HEIGHT])
 {
-
 	for(int x = 0; x < WIDTH; x++)
 	{
 		for(int y = 0; y < HEIGHT; y++)
 		{
 			int number_neighboor = count_neighbour(state, x, y);
 
-			//mort par isolement
+			//Death because not enough living cells around
 			if(state[x][y] == LIVE && number_neighboor < 2)
 				temp[x][y] = DEAD;
 
-			//reste en vie
+			//Stay alive
 			if(state[x][y] == LIVE && (number_neighboor == 2 || number_neighboor == 3))
 				temp[x][y] = LIVE;
 
-			//mort par surpopulation
+			//Death because too much cells around
 			if(state[x][y] == LIVE && number_neighboor > 3)
 				temp[x][y] = DEAD;
 
+			//Birth
 			if(state[x][y] == DEAD && number_neighboor == 3)
 				temp[x][y] = LIVE;
 		}
 	}
 
+	/*
+		Fills the color of the cells grid depending on the state array
+	*/
 	for(int x = 0; x < WIDTH; x++)
 	{
 		for(int y = 0; y < HEIGHT; y++)
@@ -142,90 +242,13 @@ void run(bool state[][HEIGHT], bool temp[][HEIGHT], sf::RectangleShape cells[][H
 	}
 }
 
-int main()
+void draw_grid(sf::RenderWindow* window, sf::RectangleShape cells[][HEIGHT])
 {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Game Of Life", sf::Style::Titlebar | sf::Style::Close);
-    window.setFramerateLimit(60);
-	sf::Event event;
-	bool state[WIDTH][HEIGHT] ;
-	bool temp[WIDTH][HEIGHT];
-	int cellX, cellY;
-	sf::RectangleShape cells[WIDTH][HEIGHT];
-	bool running = false;
-
-	init(state, temp, cells);
-	while(window.isOpen())
+	for(int x = 0; x < WIDTH; x++)
 	{
-		while(window.pollEvent(event))
+		for(int y = 0; y < HEIGHT; y++)
 		{
-			if(event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-			{
-				window.close();
-			}
-			sf::Vector2i mouse_cursor = sf::Mouse::getPosition(window);
-			cellX = mouse_cursor.x/CELL_SIZE;
-			cellY = mouse_cursor.y/CELL_SIZE;
-			if(cellX == WIDTH)
-				cellX--;
-			if(cellY == HEIGHT)
-				cellY--;
-			if((mouse_cursor.x <= SCREEN_WIDTH && mouse_cursor.y <= SCREEN_HEIGHT) && (mouse_cursor.x > 0 && mouse_cursor.y > 0))
-			{
-				if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
-				{
-					cells[cellX][cellY].setFillColor(sf::Color::Black);
-					state[cellX][cellY] = LIVE;
-				}
-
-
-				if(sf::Mouse::isButtonPressed((sf::Mouse::Right)))
-				{
-					cells[cellX][cellY].setFillColor(sf::Color::White);
-					state[cellX][cellY] = DEAD;
-				}
-			}
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::C))
-			{
-				for(int y = 0; y < HEIGHT; y++)
-				{
-					for(int x = 0; x < WIDTH; x++)
-					{
-						state[x][y] = DEAD;
-						temp[x][y] = DEAD;
-						cells[x][y].setFillColor(sf::Color::White);
-					}
-				}
-			}
-			if(sf::Keyboard::isKeyPressed((sf::Keyboard::Space)))
-				running = true;
-			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::R))
-			{
-				for(int y = 0; y < HEIGHT; y++)
-				{
-					for(int x = 0; x < WIDTH; x++)
-					{
-						state[x][y] = DEAD;
-						temp[x][y] = DEAD;
-						cells[x][y].setFillColor(sf::Color::White);
-					}
-				}
-				running = false;
-			}
-
+			window->draw(cells[x][y]);
 		}
-		if(running)
-			run(state, temp, cells);
-		window.clear();
-		for(int x = 0; x < WIDTH; x++)
-		{
-			for(int y = 0; y < HEIGHT; y++)
-			{
-				window.draw(cells[x][y]);
-			}
-		}
-		window.display();
 	}
-
-
-    return EXIT_SUCCESS;
 }
